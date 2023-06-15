9514b5ac4eba63dca836ae603be89d972c53299f
Ego <augurto.17@gmail.com>
2023-06-15 17:51:06 -0500

    logo agregado

--- inicio.php
+++ inicio.php
@@ -43,19 +43,19 @@
                         <div class="navbar-brand-box">
                             <a href="index.html" class="logo logo-dark">
                                 <span class="logo-sm">
-                                    <img src="assets/images/logo-sm-dark.png" alt="logo-sm-dark" height="22">
+                                    <img src="assets/images/adfusion.png" alt="logo-sm-dark" height="22">
                                 </span>
                                 <span class="logo-lg">
-                                    <img src="assets/images/logo-dark.png" alt="logo-dark" height="22">
+                                    <img src="assets/images/adfusion.png" alt="logo-dark" height="22">
                                 </span>
                             </a>
 
                             <a href="index.html" class="logo logo-light">
                                 <span class="logo-sm">
-                                    <img src="assets/images/logo-sm-light.png" alt="logo-sm-light" height="22">
+                                    <img src="assets/images/adfusion.png" alt="logo-sm-light" height="22">
                                 </span>
                                 <span class="logo-lg">
-                                    <img src="assets/images/logo-light.png" alt="logo-light" height="22">
+                                    <img src="assets/images/adfusion.png" alt="logo-light" height="22">
                                 </span>
                             </a>
                         </div>
