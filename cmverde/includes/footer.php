8a161f9765d14173b6ab97b1d8e747b87fa8dd73
Ego <augurto.17@gmail.com>
2023-06-15 13:58:39 -0500

    carga de plantilla

---
+++ cmverde/includes/footer.php
@@ -0,0 +1,82 @@
+
+<!-- Right Sidebar -->
+<div class="right-bar">
+    <div data-simplebar class="h-100">
+        <div class="rightbar-title d-flex align-items-center px-3 py-4">
+
+            <h5 class="m-0 me-2">Settings</h5>
+
+            <a href="javascript:void(0);" class="right-bar-toggle ms-auto">
+                <i class="mdi mdi-close noti-icon"></i>
+            </a>
+        </div>
+
+        <!-- Settings -->
+        <hr class="mt-0" />
+        <h6 class="text-center mb-0">Choose Layouts</h6>
+
+        <div class="p-4">
+            <div class="mb-2">
+                <img src="../../plantilla/assets/images/layouts/layout-1.jpg" class="img-fluid img-thumbnail" alt="layout-1">
+            </div>
+
+            <div class="form-check form-switch mb-3">
+                <input class="form-check-input theme-choice" type="checkbox" id="light-mode-switch" checked>
+                <label class="form-check-label" for="light-mode-switch">Light Mode</label>
+            </div>
+
+            <div class="mb-2">
+                <img src="../../plantilla/assets/images/layouts/layout-2.jpg" class="img-fluid img-thumbnail" alt="layout-2">
+            </div>
+            <div class="form-check form-switch mb-3">
+                <input class="form-check-input theme-choice" type="checkbox" id="dark-mode-switch"
+                    data-bsStyle="../../plantilla/assets/css/bootstrap-dark.min.css" data-appStyle="../../plantilla/assets/css/app-dark.min.css">
+                <label class="form-check-label" for="dark-mode-switch">Dark Mode</label>
+            </div>
+
+            <div class="mb-2">
+                <img src="../../plantilla/assets/images/layouts/layout-3.jpg" class="img-fluid img-thumbnail" alt="layout-3">
+            </div>
+            <div class="form-check form-switch mb-5">
+                <input class="form-check-input theme-choice" type="checkbox" id="rtl-mode-switch"
+                    data-appStyle="../../plantilla/assets/css/app-rtl.min.css">
+                <label class="form-check-label" for="rtl-mode-switch">RTL Mode</label>
+            </div>
+
+
+        </div>
+
+    </div> <!-- end slimscroll-menu-->
+</div>
+<!-- /Right-bar -->
+
+<!-- Right bar overlay-->
+<div class="rightbar-overlay"></div>
+
+<!-- JAVASCRIPT -->
+<script src="../../../plantilla/assets/libs/jquery/jquery.min.js"></script>
+<script src="../../plantilla/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../../plantilla/assets/libs/metismenu/metisMenu.min.js"></script>
+<script src="../../plantilla/assets/libs/simplebar/simplebar.min.js"></script>
+<script src="../../plantilla/assets/libs/node-waves/waves.min.js"></script>
+
+
+<!-- apexcharts -->
+<script src="../../plantilla/assets/libs/apexcharts/apexcharts.min.js"></script>
+
+<!-- jquery.vectormap map -->
+<script src="../../plantilla/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="../../plantilla/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js"></script>
+
+<!-- Required datatable js -->
+<script src="../../plantilla/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="../../plantilla/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+
+<!-- Responsive examples -->
+<script src="../../plantilla/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../../plantilla/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+
+<script src="../../plantilla/assets/js/pages/dashboard.init.js"></script>
+
+<!-- App js -->
+<script src="../../plantilla/assets/js/app.js"></script>
