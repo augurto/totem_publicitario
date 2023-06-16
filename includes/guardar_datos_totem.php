d592fcbe80d5905a2dca6756051486af800ac47b
Ego <augurto.17@gmail.com>
2023-06-16 16:00:03 -0500

    reidrecionando desde la ejeucion sql

--- includes/guardar_datos_totem.php
+++ includes/guardar_datos_totem.php
@@ -20,14 +20,11 @@ $fechaHoraPeru = date('Y-m-d H:i:s');
 $query = "INSERT INTO formulario_totem (name, email, phone, mensaje, var1, var2, var3,fecha2)
           VALUES ('$name', '$email', '$phone', '$mensaje', '$var1', '$var2', '$var3', '$fechaHoraPeru')";
 
-
 // Ejecutar la consulta
 if (mysqli_query($con, $query)) {
-    // Mostrar mensaje de éxito
-    echo 'Los datos se han guardado correctamente.';
-
-    // Puedes realizar otras acciones aquí si es necesario
-
+    // Redireccionar a la página de agradecimiento
+    header('Location: https://totempublicitario.com.pe/totem-publicitario/gracias/');
+    exit; // Asegurar que se detiene la ejecución del script después de la redirección
 } else {
     // Error al guardar los datos
     echo 'Error al guardar los datos: ' . mysqli_error($con);
