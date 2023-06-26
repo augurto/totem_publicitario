ae9f5d73b9f243996c34c4012184b0df3af8843b
Ego <augurto.17@gmail.com>
2023-06-26 18:14:57 -0500

    br antes del boton

--- inicio.php
+++ inicio.php
@@ -626,6 +626,7 @@
                                                             <option value="1">Observado</option>
                                                             <option value="2">Atendido</option>
                                                         </select>
+                                                        <br>
                                                         <button type="submit" class="btn btn-primary">Enviar</button>
                                                     </form>
 
