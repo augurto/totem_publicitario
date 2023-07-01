b590895f8dc476604633bf4face4214730deb69c
Ego <augurto.17@gmail.com>
2023-07-01 11:29:55 -0500

    cambio footer

--- inicio.php
+++ inicio.php
@@ -690,7 +690,7 @@
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    Creado por  <i class="mdi mdi-heart text-danger"></i> Ego
+                                    Creado con  <i class="mdi mdi-heart text-danger"></i>
                                 </div>
                             </div>
                         </div>
