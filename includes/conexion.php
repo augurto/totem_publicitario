29453a3dfccea2d0c0c22974052f6ce9094da1c8
Ego <augurto.17@gmail.com>
2023-07-03 15:21:53 -0500

    faltaba guardar los datos en $con

--- includes/conexion.php
+++ includes/conexion.php
@@ -4,6 +4,6 @@
    define('DB_USERNAME', "u291982824_bd_geo");
    define('DB_PASSWORD', '21.17.BDgeo');
    define('DB_DATABASE', 'u291982824_bd_geo');
-   $con=mysqli_connect('localhost','u291982824_totem','21.17.Totem','u291982824_totem');
+   $con=mysqli_connect('localhost','u291982824_bd_geo','21.17.BDgeo','u291982824_bd_geo');
    mysqli_set_charset($con, "utf8");
 ?>
