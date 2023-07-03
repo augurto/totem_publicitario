f6ecc8c44e28fdc339bdf9bb4feeef73987b25c5
Ego <augurto.17@gmail.com>
2023-07-03 15:20:26 -0500

    datos corregidos del totem a geo

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,9 +1,9 @@
 
 <?php
    define('DB_SERVER', "localhost");
-   define('DB_USERNAME', "u291982824_totem");
-   define('DB_PASSWORD', '21.17.Totem');
-   define('DB_DATABASE', 'u291982824_totem');
+   define('DB_USERNAME', "u291982824_bd_geo");
+   define('DB_PASSWORD', '21.17.BDgeo');
+   define('DB_DATABASE', 'u291982824_bd_geo');
    $con=mysqli_connect('localhost','u291982824_totem','21.17.Totem','u291982824_totem');
    mysqli_set_charset($con, "utf8");
 ?>
