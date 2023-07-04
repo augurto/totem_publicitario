e4e5be96a400b88911d0380541b73bf7eb43ce2e
Ego <augurto.17@gmail.com>
2023-07-04 18:12:27 -0500

    cambio de cerrar sesion

--- cerrar.php
+++ cerrar.php
@@ -1,11 +1,17 @@
 <?php
 session_start(); // Iniciar la sesión
 
-// Eliminar todas las variables de sesión
-$_SESSION = array();
+// Verificar si el usuario ha iniciado sesión
+if (!isset($_SESSION['usuario'])) {
+    // El usuario no ha iniciado sesión, redireccionar a la página de inicio de sesión o a otra página deseada
+    header("Location: login.php");
+    exit();
+}
+
+// Cerrar la sesión y eliminar todas las variables de sesión
+$_SESSION = array(); // Eliminar todas las variables de sesión
 
 // Destruir la sesión
-
 session_destroy();
 
 // Redireccionar a la página de inicio de sesión o a otra página deseada
