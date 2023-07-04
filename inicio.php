73d804a5be0750829d85c8ccd04d39124558f05d
Ego <augurto.17@gmail.com>
2023-07-04 13:10:13 -0500

    modal agregar cliente

--- inicio.php
+++ inicio.php
@@ -493,7 +493,37 @@
                                     
                                     <div class="page-title-center">
                                        <button type="button"
-                                        class="btn btn-info btn-rounded waves-effect waves-light">+ Cliente</button>
+                                        class="btn btn-info btn-rounded waves-effect waves-light" data-bs-toggle="modal" data-bs-target=".agregar-cliente">+ Cliente</button>
+                                        
+
+                                        <div class="modal fade agregar-cliente" tabindex="-1" role="dialog"
+                                            aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog modal-dialog-centered">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title mt-0">Center modal</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                            aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <p>Cras mattis consectetur purus sit amet fermentum.
+                                                            Cras justo odio, dapibus ac facilisis in,
+                                                            egestas eget quam. Morbi leo risus, porta ac
+                                                            consectetur ac, vestibulum at eros.</p>
+                                                        <p>Praesent commodo cursus magna, vel scelerisque
+                                                            nisl consectetur et. Vivamus sagittis lacus vel
+                                                            augue laoreet rutrum faucibus dolor auctor.</p>
+                                                        <p class="mb-0">Aenean lacinia bibendum nulla sed consectetur.
+                                                            Praesent commodo cursus magna, vel scelerisque
+                                                            nisl consectetur et. Donec sed odio dui. Donec
+                                                            ullamcorper nulla non metus auctor
+                                                            fringilla.</p>
+                                                    </div>
+                                                </div><!-- /.modal-content -->
+                                            </div><!-- /.modal-dialog -->
+                                        </div><!-- /.modal -->
+                                    </div>
+
                                         <button type="button"
                                         class="btn btn-warning btn-rounded waves-effect waves-light">+ Lead</button>
                                     </div>
