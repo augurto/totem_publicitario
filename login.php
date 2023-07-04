781778909a1f77b5b30254796476bfa4ead0060a
Ego <augurto.17@gmail.com>
2023-07-04 14:56:47 -0500

    login

--- login.php
+++ login.php
@@ -33,25 +33,25 @@
                             <div class="text-center mt-4">
                                 <div class="mb-3">
                                     <a href="index.html" class="">
-                                        <img src="assets/images/logo-dark.png" alt="" height="22" class="auth-logo logo-dark mx-auto">
-                                        <img src="assets/images/logo-light.png" alt="" height="22" class="auth-logo logo-light mx-auto">
+                                        <img src="assets/images/adfusion.png" alt="" height="22" class="auth-logo logo-dark mx-auto">
+                                        <img src="assets/images/adfusion.png" alt="" height="22" class="auth-logo logo-light mx-auto">
                                     </a>
                                 </div>
                             </div>
                             <div class="p-3">
-                                <h4 class="font-size-18 text-muted mt-2 text-center">Welcome Back !</h4>
-                                <p class="text-muted text-center mb-4">Sign in to continue to upbond.</p>
+                                <h4 class="font-size-18 text-muted mt-2 text-center">Bienvenido!</h4>
+                                <p class="text-muted text-center mb-4">Iniciar Sesion</p>
 
                                 <form class="form-horizontal" action="index.html">
 
                                     <div class="mb-3">
-                                        <label class="form-label" for="username">E-mail Address</label>
+                                        <label class="form-label" for="username">DNI</label>
                                         <input type="text" class="form-control" id="username"
                                             placeholder="Enter username">
                                     </div>
 
                                     <div class="mb-3">
-                                        <label class="form-label" for="userpassword">Password</label>
+                                        <label class="form-label" for="userpassword">Contraseña</label>
                                         <input type="password" class="form-control" id="userpassword"
                                             placeholder="Enter password">
                                     </div>
@@ -62,13 +62,13 @@
                                                 <input type="checkbox" class="form-check-input me-1"
                                                     id="customControlInline">
                                                 <label class="form-label" class="form-check-label"
-                                                    for="customControlInline">Remember me</label>
+                                                    for="customControlInline">Recuerdame</label>
                                             </div>
                                         </div>
                                         <!-- end col -->
                                         <div class="col-sm-6 text-end">
                                             <a href="auth-recoverpw.html" class="text-muted"><i
-                                                    class="mdi mdi-lock"></i> Forgot your password?</a>
+                                                    class="mdi mdi-lock"></i> Olvidaste tu contraseña?</a>
                                         </div>
                                         <!-- end col -->
                                     </div>
@@ -77,38 +77,12 @@
                                     <div class="row mb-4">
                                         <div class="col-12 text-center">
                                             <button class="btn btn-primary w-100 waves-effect waves-light"
-                                                type="submit">Log
-                                                In</button>
+                                                type="submit">Iniciar Sesion</button>
                                         </div>
                                     </div>
                                     <!-- end row -->
 
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="text-center plan-line">
-                                                or sign up with
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- end row -->
-
-                                    <div class="row mt-3">
-
-                                        <div class="button-list btn-social-icon text-center">
-                                            <button type="button" class="btn btn-facebook">
-                                                <i class="fab fa-facebook"></i>
-                                            </button>
-
-                                            <button type="button" class="btn btn-twitter ms-1">
-                                                <i class="fab fa-twitter"></i>
-                                            </button>
-
-                                            <button type="button" class="btn btn-linkedin  ms-1">
-                                                <i class="fab fa-linkedin"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <!-- end row -->
+                               
                                 </form>
                                 <!-- end form -->
                             </div>
@@ -117,11 +91,10 @@
                     </div>
                     <!-- end card -->
                     <div class="mt-5 text-center">
-                        <p>Don't have an account ?<a href="auth-register.html" class="fw-bold text-primary"> Signup
-                                Now </a></p>
+                        <p>No tienes una cuenta ?<a href="auth-register.html" class="fw-bold text-primary"> Registrate ahora                              Now </a></p>
                         <p>©
-                            <script>document.write(new Date().getFullYear())</script> Upbond. Crafted with <i
-                                class="mdi mdi-heart text-danger"></i> by Themesdesign
+                            <script>document.write(new Date().getFullYear())</script> Adfusion <i
+                                class="mdi mdi-heart text-danger"></i> 
                         </p>
                     </div>
 
