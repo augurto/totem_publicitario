2458cbce902de47a0db622d45b76853f6ae7eb48
Ego <augurto.17@gmail.com>
2023-07-04 18:08:43 -0500

    cerrar

---
+++ cerrar.php
@@ -0,0 +1,13 @@
+<?php
+session_start(); // Iniciar la sesión
+
+// Eliminar todas las variables de sesión
+$_SESSION = array();
+
+// Destruir la sesión
+session_destroy();
+
+// Redireccionar a la página de inicio de sesión o a otra página deseada
+header("Location: login.php");
+exit();
+?>
