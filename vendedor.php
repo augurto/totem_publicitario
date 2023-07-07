e8430a6e616c4757f2b6671d9fad99705c1580e1
Ego <augurto.17@gmail.com>
2023-07-07 15:49:28 -0500

    agrupando mejor

--- vendedor.php
+++ vendedor.php
@@ -568,8 +568,9 @@ $tipoUsuario = $_SESSION['tipoUsuario'];
                                                 }
 
                                                 // Consulta SQL para obtener los datos de la tabla "formulario_totem"
-                                                $sql = "SELECT * FROM web_formularios GROUP BY documentoCliente ORDER BY fecha DESC;
-                                                ";
+                                                $sql = "SELECT * FROM web_formularios GROUP BY documentoCliente ORDER BY fecha DESC";
+                                              
+
                                                 $result = $conn->query($sql);
 
                                                 // Verificar si se obtuvieron resultados
