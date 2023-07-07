390b18dbe635b248e9a99f16842c9994bfd0c13e
Ego <augurto.17@gmail.com>
2023-07-07 10:28:31 -0500

    prueba casi final form

--- includes/guardar_webform.php
+++ includes/guardar_webform.php
@@ -1,24 +1,28 @@
+
 <?php
 include 'conexion.php'; // Incluir el archivo de conexión
 
 // Obtener los datos enviados desde el formulario
-$idCliente = $_POST['idcliente'];
+$documentoCliente = $_POST['idcliente'];
 $tipoCliente = $_POST['tipoCliente'];
 $prospecto = $_POST['prospecto'];
-$observacion = $_POST['observacion'];
+$observacionCliente = $_POST['observacion'];
 $idid = $_POST['idid'];
-$iduser = $_POST['iduser'];
+$id_user = $_POST['iduser'];
 
 // Preparar la consulta SQL para realizar la inserción
-$query = "INSERT INTO web_formularios (documentoCliente,tipoCliente, prospecto, observacionCliente, idid, id_user) VALUES ('$idCliente', '$tipoCliente', '$prospecto', '$observacion', '$idid', '$iduser')";
+$query = "INSERT INTO web_formularios (documentoCliente,tipoCliente, prospecto, observacionCliente, idid, id_user) VALUES ('$documentoCliente','$tipoCliente', '$prospecto', '$observacionCliente', '$idid', '$id_user')";
 
 // Ejecutar la consulta y verificar si se realizó correctamente
 if (mysqli_query($con, $query)) {
-    // Obtén el último id_form_web generado
+    // La inserción fue exitosa, obtén el id_form_web generado
     $id_web = mysqli_insert_id($con);
 
+    // Guardar el valor de id_form_web en la variable $id_web
+    $id_web = $id_web;
+
     // Redirecciona a la página cliente.php con el id_form_web como parámetro en la URL
-    header("Location: cliente.php?id=" . $id_web);
+    header("Location: ../cliente.php?id=" . $id_web);
     exit();
 } else {
     // Ocurrió un error durante la inserción, puedes enviar un mensaje de error al cliente si lo deseas
