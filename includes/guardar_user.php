dd5d45264555feccec67cdc1833366f822f81dbc
Ego <augurto.17@gmail.com>
2023-07-07 10:42:24 -0500

    redireccionando luego de guardar

--- includes/guardar_user.php
+++ includes/guardar_user.php
@@ -1,3 +1,4 @@
+
 <?php
 // Incluir el archivo de conexión a la base de datos
 include 'conexion.php';
@@ -12,13 +13,18 @@ $idweb = $_POST['idweb'];
 $iduser = $_POST['iduser'];
 
 // Realizar la consulta para insertar los datos en la base de datos
-$query = "INSERT INTO cliente (datosCliente, documentoCliente, 	telefonoCliente, emailCliente, mensajeCliente, 	idWeb, 	idUsuarioMake) VALUES ('$datos', '$documento', '$telefono', '$email', '$comentario', '$idweb', '$iduser')";
+$query = "INSERT INTO cliente (datosCliente, documentoCliente, telefonoCliente, emailCliente, mensajeCliente, idWeb, idUsuarioMake) VALUES ('$datos', '$documento', '$telefono', '$email', '$comentario', '$idweb', '$iduser')";
 $result = mysqli_query($con, $query);
 
 // Verificar si la consulta fue exitosa
 if ($result) {
-    // La inserción de datos fue exitosa, puedes mostrar algún mensaje o realizar alguna acción adicional si lo deseas
-    echo "Datos guardados correctamente";
+    // La inserción de datos fue exitosa
+    // Obtener el último ID generado
+    $lastId = mysqli_insert_id($con);
+
+    // Redireccionar a la misma página con el parámetro de ID en la URL
+    header("Location: cliente.php?id=" . $lastId);
+    exit();
 } else {
     // Ocurrió un error durante la inserción de datos, puedes mostrar un mensaje de error o realizar alguna acción adicional si lo deseas
     echo "Error al guardar los datos";
