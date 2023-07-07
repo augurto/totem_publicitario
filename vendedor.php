601f8c4a1674b132e91063a8bd13a3f5243ea95f
Ego <augurto.17@gmail.com>
2023-07-07 15:33:09 -0500

    agregar lead boton

--- vendedor.php
+++ vendedor.php
@@ -509,7 +509,12 @@ $tipoUsuario = $_SESSION['tipoUsuario'];
                             <div class="col-12">
                                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                                     <h4 class="mb-sm-0">Data Tables</h4>
+                                    <div class="page-title-center">
+                                    <button type="button" class="btn btn-primary waves-effect waves-light" onclick="window.location.href = 'cliente.php';">
+                                        Lead <i class="mdi mdi-emoticon-excited-outline font-size-16 align-middle ms-2"></i>
+                                    </button>
 
+                                    </div>
                                     <div class="page-title-right">
                                         <ol class="breadcrumb m-0">
                                             <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
