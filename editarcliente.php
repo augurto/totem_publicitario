9e3c8f60763e2436b8bf9cf2f697f1d6afb098a9
Ego <augurto.17@gmail.com>
2023-07-09 18:23:49 -0500

    igualanto por id

--- editarcliente.php
+++ editarcliente.php
@@ -663,7 +663,7 @@ mysqli_close($con);
                                                 <?php
                                                  include 'includes/conexion.php'; 
                                                 // Realizar la consulta a la base de datos para obtener los datos de la tabla
-                                                $queryc = "SELECT * FROM cliente order by idCliente DESC    ";
+                                                $queryc = "SELECT * FROM cliente WHERE id_form_web=$idUrl ";
                                                 $resultc = mysqli_query($con, $queryc);
 
                                                 // Verificar si se encontraron resultados
