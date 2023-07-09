2a913f926eb697ceceaed88e13236f4a8714d8e9
Ego <augurto.17@gmail.com>
2023-07-09 12:35:27 -0500

    seguimeinto

--- editarcliente.php
+++ editarcliente.php
@@ -660,12 +660,10 @@ mysqli_close($con);
                                                 <label class="form-label">Buscar Cliente</label>
                                                 
                                                 <select class="form-control select2" id="idcliente" name="idcliente">
-                                                
                                                 <?php
                                                  include 'includes/conexion.php'; 
                                                 // Realizar la consulta a la base de datos para obtener los datos de la tabla
-                                                
-                                                $queryc = "SELECT * FROM cliente where documentoCliente= $documento";
+                                                $queryc = "SELECT * FROM cliente order by idCliente DESC    ";
                                                 $resultc = mysqli_query($con, $queryc);
 
                                                 // Verificar si se encontraron resultados
