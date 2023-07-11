f3e8f246df121692efc40f320ab71501881269e7
Ego <augurto.17@gmail.com>
2023-07-11 18:13:20 -0500

    mostrando valor de $documentoCliente

--- editarcliente.php
+++ editarcliente.php
@@ -667,9 +667,10 @@ mysqli_close($con);
 
                                             <input type="text" id="pr" name="pr" class="form-control" value="<?php echo $_GET['pr'] ; ?>" readonly>
                                             <br>
-                                            <?php if ($documento === null || empty($documentoCliente)): ?>
+                                            <?php if ($documento === null || empty($documentoCliente)):  echo $documentoCliente
+;?>
                                                 <center>
-                                                    <button type="submit" id="submitBtn" class="btn btn-outline-success btn-rounded waves-effect waves-light">Actualizar Datos</button>
+                                                                                                    <button type="submit" id="submitBtn" class="btn btn-outline-success btn-rounded waves-effect waves-light">Actualizar Datos</button>
                                                 </center>
                                             <?php elseif($documentoCliente !== null): ?>
                                                 <p>Documentos Completos</p>
