b88aa7fb4c654900e86bdd46b1cbf9a6fdd84224
Ego <augurto.17@gmail.com>
2023-07-17 00:39:07 -0500

    sin comillas simple

--- reporteVentas.php
+++ reporteVentas.php
@@ -513,7 +513,7 @@ include 'includes/conexion.php'; // Incluir el archivo de conexión
 
                         // Realizar la consulta para obtener los datos de la tabla "venta"
                         $query = "SELECT idVenta, idProducto, nombreProducto, precioProducto, cantidadProducto, montoAdicional, montoTotal, id_web_formularios, rutaArchivo, fechaVenta, estadoVenta, idUser, nombreArchivo, observacionVenta 
-                        FROM ventas WHERE idUser ='$idUser' ";
+                        FROM ventas WHERE idUser =$idUser ";
                         $result = mysqli_query($con, $query);
 
                         // Verificar si se obtuvieron resultados
