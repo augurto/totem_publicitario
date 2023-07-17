e96bd01e410abb7784d6ef9fcb1739012bf6f4a1
Ego <augurto.17@gmail.com>
2023-07-17 02:48:05 -0500

    quitan script de datos

--- reporteVentasGeneral.php
+++ reporteVentasGeneral.php
@@ -934,7 +934,7 @@ $tipoUsuario = $_SESSION['tipoUsuario'];
 <script src="assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
 <script src="assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
 
-<script src="assets/js/pages/dashboard.init.js"></script>
+<!-- <script src="assets/js/pages/dashboard.init.js"></script> -->
 
 
 </body>
