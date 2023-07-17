1997d4c1e8a0319df97be0dfe22284dc4e27e8a8
Ego <augurto.17@gmail.com>
2023-07-17 02:06:50 -0500

    no atendidos

--- vendedor.php
+++ vendedor.php
@@ -12,8 +12,17 @@ $usuario = $_SESSION['usuario'];
 $dni = $_SESSION['dni'];
 $tipoUsuario = $_SESSION['tipoUsuario'];
 
-// Resto del código de la página
-// ...
+
+$queryNoAtendidos = "SELECT COUNT(*) AS countNoAtendidos FROM web_formularios WHERE estado_web = 0";
+$resultNoAtendidos = mysqli_query($con, $queryNoAtendidos);
+
+if ($resultNoAtendidos) {
+    $rowNoAtendidos = mysqli_fetch_assoc($resultNoAtendidos);
+    $noAtendidos = $rowNoAtendidos['countNoAtendidos'];
+} else {
+    $noAtendidos = 0; // Si hay un error en la consulta, establecemos el valor en 0
+}
+
 
 ?>
 
