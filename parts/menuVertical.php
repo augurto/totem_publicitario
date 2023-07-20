6a49398d6c865823ec6538884e7c1e361e5a429f
Ego <augurto.17@gmail.com>
2023-07-19 19:17:00 -0500

    reporteVentas

--- parts/menuVertical.php
+++ parts/menuVertical.php
@@ -85,10 +85,33 @@ mysqli_close($con);
                 ?>
 
             </li>
-            
-           
-         
-           
+            <!-- end li -->
+            <li>
+            <?php
+                if ($tipoUsuario == 3) {
+                    echo '<a href="reporteEmpresa.php" class="waves-effect">
+                            <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">'.$vendidos2.'</span>
+                            <span>Reportes</span>
+                        </a>';
+                } elseif ($tipoUsuario == 2) {
+                    echo '<a href="reporteVentasGeneral.php" class="waves-effect">
+                            <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">'.$vendidos2.'</span>
+                            <span>Reportes</span>
+                        </a>';
+                } elseif ($tipoUsuario == 1) {
+                    echo '<a href="reporteVentas.php" class="waves-effect">
+                            <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">'.$vendidos2.'</span>
+                            <span>Reportes</span>
+                        </a>';
+                } else {
+                    echo '<a href="Reportes.php" class="waves-effect">
+                            <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">'.$vendidos2.'</span>
+                            <span>Reportes</span>
+                        </a>';
+                }
+                ?>
+
+            </li>
             <!-- end li -->
         </ul>
         <!-- end ul -->
