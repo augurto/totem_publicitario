aa58cf4abc8a8cff77922aef62c12ccbbc354c35
Ego <augurto.17@gmail.com>
2023-07-21 09:35:18 -0500

    as

--- seguimientoCliente.php
+++ seguimientoCliente.php
@@ -133,8 +133,107 @@ mysqli_close($con);
             
         <?php
         include './parts/nav.php';
-        include './parts/menuVertical.php'
+       /*  include './parts/menuVertical.php' */
         ?>
+        
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu">
+
+<div data-simplebar class="h-100">
+
+    <!--- Sidemenu -->
+    <div id="sidebar-menu">
+        <!-- Left Menu Start -->
+        <ul class="metismenu list-unstyled" id="side-menu">
+            <li class="menu-title">Menu</li>
+
+            <li>
+                <?php
+                if ($tipoUsuario == 3) {
+                    echo '<a href="inicio.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">' . $noAtendidos . '</span>
+                        <span>Inicio</span>
+                    </a>';
+                } elseif ($tipoUsuario == 2) {
+                    echo '<a href="administrador.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">' . $noAtendidos . '</span>
+                        <span>Inicio</span>
+                    </a>';
+                } elseif ($tipoUsuario == 1) {
+                    echo '<a href="vendedor.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">' . $noAtendidos . '</span>
+                        <span>Inicio</span>
+                    </a>';
+                } else {
+                    echo '<a href="login.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">' . $noAtendidos . '</span>
+                        <span>Inicio</span>
+                    </a>';
+                }
+                ?>
+            </li>
+            <!-- end li -->
+            <li>
+                <?php
+                if ($tipoUsuario == 3) {
+                    echo '<a href="vendidos.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">' . $vendidos . '</span>
+                        <span>Vendidos</span>
+                    </a>';
+                } elseif ($tipoUsuario == 2) {
+                    echo '<a href="vendidos.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">' . $vendidos . '</span>
+                        <span>Vendidos</span>
+                    </a>';
+                } elseif ($tipoUsuario == 1) {
+                    echo '<a href="vendidos.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">' . $vendidos . '</span>
+                        <span>Vendidos</span>
+                    </a>';
+                } else {
+                    echo '<a href="vendidos.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">' . $vendidos . '</span>
+                        <span>Vendidos</span>
+                    </a>';
+                }
+                ?>
+
+            </li>
+            <!-- end li -->
+            <li>
+                <?php
+                if ($tipoUsuario == 3) {
+                    echo '<a href="reporteEmpresa.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i>
+                        <span>Reportes</span>
+                    </a>';
+                } elseif ($tipoUsuario == 2) {
+                    echo '<a href="reporteVentasGeneral.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i>
+                        <span>Reportes</span>
+                    </a>';
+                } elseif ($tipoUsuario == 1) {
+                    echo '<a href="reporteVentas.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i>
+                        <span>Reportes</span>
+                    </a>';
+                } else {
+                    echo '<a href="Reportes.php" class="waves-effect">
+                        <i class="ri-dashboard-line"></i>
+                        <span>Reportes</span>
+                    </a>';
+                }
+                ?>
+
+            </li>
+            <!-- end li -->
+        </ul>
+        <!-- end ul -->
+    </div>
+    <!-- Sidebar -->
+</div>
+</div>
+<!-- Left Sidebar End -->
     <!-- ============================================================== -->
     <!-- Start right Content here -->
     <!-- ============================================================== -->
