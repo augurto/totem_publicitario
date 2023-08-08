29fe4c5a55ad7f7a3b1898f93f09ac6c344eb2a3
Ego <augurto.17@gmail.com>
2023-08-08 01:27:54 -0500

    borrado gitignore

--- includes/conexion.php
+++ includes/conexion.php
@@ -3,6 +3,7 @@
    define('DB_USERNAME', "u291982824_prueba");
    define('DB_PASSWORD', '21.17.Prueba');
    define('DB_DATABASE', 'u291982824_prueba');
+   
    $con=mysqli_connect('localhost','u291982824_prueba','21.17.Prueba','u291982824_prueba');
    mysqli_set_charset($con, "utf8");
 ?>
