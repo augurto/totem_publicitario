16b9c6b30b4e6e22a9105b906cea44b87bf6e2f9
Ego <augurto.17@gmail.com>
2023-08-08 01:14:58 -0500

    leads

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,9 +1,8 @@
-
 <?php
    define('DB_SERVER', "localhost");
-   define('DB_USERNAME', "u291982824_bd_geo");
-   define('DB_PASSWORD', '21.17.BDgeo');
-   define('DB_DATABASE', 'u291982824_bd_geo');
-   $con=mysqli_connect('localhost','u291982824_bd_geo','21.17.BDgeo','u291982824_bd_geo');
+   define('DB_USERNAME', "u291982824_prueba");
+   define('DB_PASSWORD', '21.17.Prueba');
+   define('DB_DATABASE', 'u291982824_prueba');
+   $con=mysqli_connect('localhost','u291982824_prueba','21.17.Prueba','u291982824_prueba');
    mysqli_set_charset($con, "utf8");
 ?>
