13c046a40fca018107d168c4e40f6c655f97c940
Ego <augurto.17@gmail.com>
2023-08-08 13:51:29 -0500

    subida de slack

---
+++ mensajeSlack.php
@@ -0,0 +1,23 @@
+<script>
+    // Post a message to a channel your app is in using ID and message text
+async function publishMessage(id, text) {
+  try {
+    // Call the chat.postMessage method using the built-in WebClient
+    const result = await app.client.chat.postMessage({
+      // The token you used to initialize your app
+      token: "xoxb-16060872976-5733378532096-qCQAWZgNTKnKuPhYKv7A5JwR",
+      channel: id,
+      text: text
+      // You could also use a blocks[] array to send richer content
+    });
+
+    // Print result, which includes information about the message (like TS)
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+publishMessage("C05G7CUT2GG", "Mensaje  :tada:");
+</script>
