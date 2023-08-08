bb3d4e8ca767be7987d7ec75c5568d69d4d74977
Ego <augurto.17@gmail.com>
2023-08-08 15:20:28 -0500

    otro mensaje

--- mensajeSlack.php
+++ mensajeSlack.php
@@ -1,39 +1,28 @@
 <?php
+$token = "xoxb-16060872976-5733378532096-ME5NLPxqlG7UDjsMx0jguCKM";
+$channel = "C05G7CUT2GG";
+$message = "Hola, este es un mensaje enviado desde PHP.";
 
-// Datos de tu aplicación en Slack
-$slackToken = 'xoxb-16060872976-5733378532096-ME5NLPxqlG7UDjsMx0jguCKM';  // Reemplaza con tu token de acceso
-$channel = 'C05LF94CS07';  // Reemplaza con el ID del canal de destino
-$message = 'tada';  // El mensaje que deseas enviar
-
-// Construye los datos para la solicitud POST
-$data = http_build_query([
-    'token' => $slackToken,
+$data = array(
+    'token' => $token,
     'channel' => $channel,
-    'text' => $message,
-]);
+    'text' => $message
+);
 
-// Configura la solicitud HTTP POST
-$options = [
-    'http' => [
-        'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
-        'method' => 'POST',
-        'content' => $data,
-    ],
-];
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ),
+);
 
-// Realiza la solicitud HTTP POST
-$context = stream_context_create($options);
-$response = file_get_contents('https://slack.com/api/chat.postMessage', false, $context);
+$context  = stream_context_create($options);
+$result = file_get_contents('https://slack.com/api/chat.postMessage', false, $context);
 
-// Procesa la respuesta (puede ser en formato JSON)
-if ($response) {
-    $responseData = json_decode($response, true);
-    if ($responseData['ok']) {
-        echo 'Mensaje enviado exitosamente.';
-    } else {
-        echo 'Error al enviar el mensaje: ' . $responseData['error'];
-    }
+if ($result === FALSE) { 
+    echo "Ha ocurrido un error al enviar el mensaje.";
 } else {
-    echo 'Error al realizar la solicitud.';
+    echo "Mensaje enviado correctamente.";
 }
 ?>
