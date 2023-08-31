a2c934db2ffc7819b6ed2cbbd32ec617721fbd42
Ego <augurto.17@gmail.com>
2023-08-30 22:49:48 -0500

    nuevoClienteGeo

---
+++ includes/nuevoClienteGeo.php
@@ -0,0 +1,37 @@
+<?php
+include 'conexion.php';  // Reemplaza con la ruta correcta de tu archivo de conexión
+
+// Obtener el último número disponible
+$sql = "SELECT MAX(SUBSTRING(ClienteID, 10)) AS ultimo_numero FROM Cliente";
+$result = mysqli_query($con, $sql);
+$ultimo_numero = 0;
+
+if ($result) {
+    $row = mysqli_fetch_assoc($result);
+    $ultimo_numero = intval($row['ultimo_numero']);
+}
+
+// Generar el siguiente número
+$siguiente_numero = $ultimo_numero + 1;
+$numero_formateado = str_pad($siguiente_numero, 6, '0', STR_PAD_LEFT);
+$cliente_id = "CL-GEO-" . $numero_formateado;
+
+// Obtener los datos del formulario
+$DniCliente = $_POST['DniCliente'];
+$Nombre = $_POST['Nombre'];
+$Apellido = $_POST['Apellido'];
+$FechaNacimiento = $_POST['FechaNacimiento'];
+$Genero = $_POST['Genero'];
+
+// Insertar los datos en la base de datos
+$sql = "INSERT INTO Cliente (ClienteID, DniCliente, Nombre, Apellido, FechaNacimiento, Genero)
+        VALUES ('$cliente_id', '$DniCliente', '$Nombre', '$Apellido', '$FechaNacimiento', '$Genero')";
+
+if (mysqli_query($con, $sql)) {
+    echo "Cliente registrado exitosamente";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+}
+
+mysqli_close($con);
+?>
---
+++ nuevoClienteGeo.php
@@ -0,0 +1,195 @@
+<?php
+session_start();
+include 'includes/conexion.php'; // Incluir el archivo de conexión
+
+if (!isset($_SESSION['usuario'])) {
+    // El usuario no ha iniciado sesión, redireccionar a la página de inicio de sesión o mostrar un mensaje de error
+    header("Location: login.php");
+    exit();
+}
+
+
+$usuario = $_SESSION['usuario'];
+$dni = $_SESSION['dni'];
+$tipoUsuario = $_SESSION['tipoUsuario'];
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Atencion Cliente</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesdesign" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <link href="assets/libs/select2/css/select2.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/libs/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+    <link href="assets/libs/spectrum-colorpicker2/spectrum.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet">
+
+    <!-- Bootstrap Css -->
+    <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+</head>
+<style>
+    @media print {
+        body {
+            display: none;
+        }
+    }
+</style>
+
+<body data-topbar="dark">
+    <!-- <body data-layout="horizontal" data-topbar="dark"> -->
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+
+        <?php
+        include './parts/nav.php';
+        include './parts/menuVertical.php'
+        ?>
+
+
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <div class="page-content">
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                <h4 class="mb-sm-0">Registrar Cliente </h4>
+
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Home</a></li>
+                                        <li class="breadcrumb-item active">Cliente</li>
+                                    </ol>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+
+                    <div class="row">
+                        <div class="col-lg-8">
+
+
+                            <div class="card">
+                                <div class="card-body">
+
+
+                                    <br>
+
+                                    <form action="./includes/nuevoClienteGeo.php" method="POST">
+                                        <div class="row mb-6">
+                                            <label for="DniCliente" class="col-sm-2 col-form-label">Documento</label>
+                                            <div class="col-sm-10">
+                                                <input class="form-control" type="text" id="DniCliente" name="DniCliente" maxlength="20" required>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-6">
+                                            <label for="Nombre" class="col-sm-2 col-form-label">Nombre</label>
+                                            <div class="col-sm-10">
+                                                <input class="form-control" type="text" id="Nombre" name="Nombre" maxlength="50" required>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-6">
+                                            <label for="Apellido" class="col-sm-2 col-form-label">Apellido</label>
+                                            <div class="col-sm-10">
+                                                <input class="form-control" type="text" id="Apellido" name="Apellido" maxlength="50" required>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-6">
+                                            <label for="FechaNacimiento" class="col-sm-2 col-form-label">Fecha de Nacimiento</label>
+                                            <div class="col-sm-10">
+                                                <input class="form-control" type="date" id="FechaNacimiento" name="FechaNacimiento">
+                                            </div>
+                                        </div>
+                                        <div class="row mb-6">
+                                            <label for="Genero" class="col-sm-2 col-form-label">Género</label>
+                                            <div class="col-sm-10">
+                                                <select class="form-control" id="Genero" name="Genero">
+                                                    <option value="Masculino">Masculino</option>
+                                                    <option value="Femenino">Femenino</option>
+                                                    <option value="Otro">Otro</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <!-- Otros campos aquí: CorreoElectronico, Telefono, Direccion, etc. -->
+                                        <div class="row mb-6">
+                                            <div class="col-sm-10 offset-sm-2">
+                                                <button type="submit" class="btn btn-primary">Registrar Cliente</button>
+                                            </div>
+                                        </div>
+                                    </form>
+
+                                </div>
+                                <!-- end cardbody -->
+                            </div>
+                            <!-- end card -->
+                        </div>
+                        <!-- end col -->
+
+                        
+
+                    </div>
+                    <!-- end col -->
+
+
+                </div>
+                <!-- end row -->
+
+
+            </div>
+            <!-- container-fluid -->
+        </div>
+        <!-- End Page-content -->
+        <?php include './parts/footer.php'; ?>
+    </div>
+    <!-- end main content-->
+    </div>
+    <!-- END layout-wrapper -->
+
+    <?php include './parts/sidebar.php'; ?>
+
+    <!-- Right bar overlay-->
+    <div class="rightbar-overlay"></div>
+
+    <!-- JAVASCRIPT -->
+    <script src="assets/libs/jquery/jquery.min.js"></script>
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+    <script src="assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="assets/libs/node-waves/waves.min.js"></script>
+
+    <script src="assets/libs/select2/js/select2.min.js"></script>
+    <script src="assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+    <script src="assets/libs/spectrum-colorpicker2/spectrum.min.js"></script>
+    <script src="assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+    <script src="assets/libs/admin-resources/bootstrap-filestyle/bootstrap-filestyle.min.js"></script>
+    <script src="assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js"></script>
+    <script src="assets/js/pages/form-advanced.init.js"></script>
+    <script src="assets/js/app.js"></script>
+
+</body>
+
+</html>
