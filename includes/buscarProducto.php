853e66ee8ec0fcc4a5112df81126cb0a64770f9d
Ego <augurto.17@gmail.com>
2023-09-15 16:39:30 -0500

    doble consulta

--- includes/buscarProducto.php
+++ includes/buscarProducto.php
@@ -5,26 +5,32 @@ if (isset($_POST['atributos'])) {
     $selectedAtributos = $_POST['atributos'];
     $atributosCondition = implode(',', $selectedAtributos);
 
-    // Consulta para obtener el producto con la mayoría de los atributos seleccionados
-    $query = "SELECT p.Nombre, p.Precio
+    // Consulta para obtener el último producto que cumple con la mayoría de los requisitos
+    $query = "SELECT p.ID, p.Nombre, COUNT(pa.ID) AS contador
               FROM productos p
-              WHERE p.ID IN (
-                  SELECT pa.Producto_ID
-                  FROM producto_atributos pa
-                  WHERE pa.Atributo_ID IN ($atributosCondition)
-                  GROUP BY pa.Producto_ID
-                  HAVING COUNT(DISTINCT pa.Atributo_ID) = " . count($selectedAtributos) . "
-              )
+              INNER JOIN producto_atributos pa ON p.ID = pa.Producto_ID
+              WHERE pa.Atributo_ID IN ($atributosCondition)
+              GROUP BY p.ID
+              ORDER BY contador DESC, p.ID DESC
               LIMIT 1";
 
     $result = mysqli_query($con, $query);
 
     if ($row = mysqli_fetch_assoc($result)) {
-        $producto = $row['Nombre'];
-        $precio = $row['Precio'];
-        echo $producto . " - Precio: $" . $precio;
-    } else {
-        echo 'Ningún producto coincide con los atributos seleccionados.';
+        $productoID = $row['ID'];
+        $productoNombre = $row['Nombre'];
+
+        // Consulta para obtener el precio del producto
+        $precioQuery = "SELECT Precio FROM productos WHERE ID = $productoID";
+        $precioResult = mysqli_query($con, $precioQuery);
+
+        if ($precioRow = mysqli_fetch_assoc($precioResult)) {
+            $precio = $precioRow['Precio'];
+        } else {
+            $precio = "Precio no disponible";
+        }
+
+        echo "Nombre del Producto: $productoNombre - Precio: $precio";
     }
 
     mysqli_free_result($result);
