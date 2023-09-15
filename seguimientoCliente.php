5b74b92962162eeb8f3f736b2310a9c451bbfd4f
Ego <augurto.17@gmail.com>
2023-09-15 18:47:09 -0500

    asd

--- seguimientoCliente.php
+++ seguimientoCliente.php
@@ -427,6 +427,7 @@ $dni = $_SESSION['dni'];
                                             <div>
                                                 <strong>Total: </strong><span id="total">0</span>
                                             </div>
+                                            
 
                                             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
                                             <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
