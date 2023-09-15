0b7872a5c75a15ac0bd650d7148d97ef28156cfe
Ego <augurto.17@gmail.com>
2023-09-15 16:09:36 -0500

    producto

--- includes/buscarProducto.php
+++ includes/buscarProducto.php
@@ -5,21 +5,24 @@ if (isset($_POST['atributos'])) {
     $selectedAtributos = $_POST['atributos'];
     $atributosCondition = implode(',', $selectedAtributos);
 
-    // Consulta para obtener el precio del producto
-    $query = "SELECT p.Precio
+    // Consulta para obtener el último producto que cumple con la mayoría de los requisitos
+    $query = "SELECT p.Nombre, COUNT(pa.ID) AS contador
               FROM productos p
               INNER JOIN producto_atributos pa ON p.ID = pa.Producto_ID
               WHERE pa.Atributo_ID IN ($atributosCondition)
-              GROUP BY p.ID";
+              GROUP BY p.ID
+              ORDER BY contador DESC, p.ID DESC
+              LIMIT 1";
 
     $result = mysqli_query($con, $query);
 
     if ($row = mysqli_fetch_assoc($result)) {
-        $precio = $row['Precio'];
-        echo $precio;
+        $producto = $row['Nombre'];
+        echo $producto;
     }
 
     mysqli_free_result($result);
     mysqli_close($con);
 }
+
 ?>
