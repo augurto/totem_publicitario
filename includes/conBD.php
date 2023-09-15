72b2ee18aece49932fd3b71a10abb565a6d8d970
Ego <augurto.17@gmail.com>
2023-09-15 17:16:53 -0500

    ad

--- includes/conBD.php
+++ includes/conBD.php
@@ -1,31 +1,30 @@
 <?php
+// Conexión al servidor LDAP
+$ldapconn = ldap_connect("ldap://10.0.4.8:389") or die("No se pudo conectar al servidor LDAP.");
 
-// Datos de conexión LDAP
-$ldap_servidor = "ldap://10.0.4.8:389";
-$ldap_puerto = 389;
-$ldap_base_busqueda = "ou=employees,dc=geosatelital,dc=org";
-$ldap_usuario = "cn=admin,dc=geosatelital,dc=org";
-$ldap_contrasena = "7kw7KXHAHo";
+// Autenticación en el servidor LDAP
+$ldapbind = ldap_bind($ldapconn, "cn=admin,dc=geosatelital,dc=org", "7kw7KXHAHo") or die("No se pudo autenticar en el servidor LDAP.");
 
-// Intentar la conexión LDAP
-$ldap_conexion = ldap_connect($ldap_servidor, $ldap_puerto);
+// Búsqueda en el servidor LDAP
+$basedn = "ou=employees,dc=geosatelital,dc=org";
+$filter = "(|(employeeType=admin)(employeeType=sistemas))";
+$attributes = array("uid", "cn", "sn", "mail");
+$search = ldap_search($ldapconn, $basedn, $filter, $attributes);
+$info = ldap_get_entries($ldapconn, $search);
 
-if (!$ldap_conexion) {
-    die("No se pudo conectar al servidor LDAP.");
+// Mostrar los resultados de la búsqueda
+echo "<table>";
+echo "<tr><th>Nombre de usuario</th><th>Nombre</th><th>Apellidos</th><th>Correo electrónico</th></tr>";
+for ($i = 0; $i < $info["count"]; $i++) {
+    echo "<tr>";
+    echo "<td>" . $info[$i]["uid"][0] . "</td>";
+    echo "<td>" . $info[$i]["cn"][0] . "</td>";
+    echo "<td>" . $info[$i]["sn"][0] . "</td>";
+    echo "<td>" . $info[$i]["mail"][0] . "</td>";
+    echo "</tr>";
 }
+echo "</table>";
 
-// Establecer opciones LDAP
-ldap_set_option($ldap_conexion, LDAP_OPT_PROTOCOL_VERSION, 3);
-ldap_set_option($ldap_conexion, LDAP_OPT_REFERRALS, 0);
-
-// Intentar la autenticación
-if (ldap_bind($ldap_conexion, $ldap_usuario, $ldap_contrasena)) {
-    echo "Conexión LDAP exitosa.";
-} else {
-    echo "Error de conexión LDAP: " . ldap_error($ldap_conexion);
-}
-
-// Cerrar la conexión LDAP
-ldap_close($ldap_conexion);
-
+// Cerrar la conexión al servidor LDAP
+ldap_close($ldapconn);
 ?>
