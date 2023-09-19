d9ff89ea38331a7c5779320b38e56b59c918c4cc
Ego <augurto.17@gmail.com>
2023-09-19 12:57:48 -0500

    sin duplicados

--- reporteMKT.php
+++ reporteMKT.php
@@ -292,13 +292,20 @@ $tipoUsuario = $_SESSION['tipoUsuario'];
         <!-- END layout-wrapper -->
 
         <?php include './parts/sidebar.php'; ?>
-
-        <!-- JAVASCRIPT -->
+       <!-- JAVASCRIPT -->
         <script src="assets/libs/jquery/jquery.min.js"></script>
         <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
         <script src="assets/libs/metismenu/metisMenu.min.js"></script>
         <script src="assets/libs/simplebar/simplebar.min.js"></script>
         <script src="assets/libs/node-waves/waves.min.js"></script>
+        <script src="assets/libs/select2/js/select2.min.js"></script>
+        <script src="assets/libs/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+        <script src="assets/libs/spectrum-colorpicker2/spectrum.min.js"></script>
+        <script src="assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+        <script src="assets/libs/admin-resources/bootstrap-filestyle/bootstrap-filestyle.min.js"></script>
+        <script src="assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js"></script>
+        <script src="assets/js/pages/form-advanced.init.js"></script>
+        <script src="assets/js/app.js"></script>
 
         <!-- Required datatable js -->
         <script src="assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
@@ -322,7 +329,6 @@ $tipoUsuario = $_SESSION['tipoUsuario'];
 
         <!-- Datatable init js -->
         <script src="assets/js/pages/datatables.init.js"></script>
-
         <script src="assets/js/app.js"></script>
 
 </body>
