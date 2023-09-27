5a74bb3e95c347f5bd3eac10ecd33beaea852727
Ego <augurto.17@gmail.com>
2023-09-27 03:21:05 -0500

    xcv

--- assets/js/pages/apexcharts.init.js
+++ assets/js/pages/apexcharts.init.js
@@ -146,23 +146,20 @@ options = {
   options
 )).render();
 /* inicio de datos de grafica */
-options = {
+// Supongamos que jsonData contiene los datos obtenidos de la base de datos en formato JSON
+
+var options = {
   chart: { height: 350, type: "bar", toolbar: { show: !1 } },
   plotOptions: {
     bar: { horizontal: !1, columnWidth: "45%", endingShape: "rounded" },
   },
   dataLabels: { enabled: !1 },
   stroke: { show: !0, width: 2, colors: ["transparent"] },
-  series: [
-    { name: "Net Profit", data: [46, 57, 59, 54, 62, 58, 64, 60, 66] },
-    { name: "Revenue", data: [74, 83, 102, 97, 86, 106, 93, 114, 94] },
-    { name: "Free Cash Flow", data: [37, 42, 38, 26, 47, 50, 54, 55, 43] },
-  ],
   colors: ["#5867c3", "#34c38f", "#f9c341"],
   xaxis: {
-    categories: ["Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+    categories: jsonData.map(item => item.mes_anio), // Utiliza los meses y años como etiquetas
   },
-  yaxis: { title: { text: "$ (thousands)" } },
+  yaxis: { title: { text: "$ (thousands2)" } },
   grid: { borderColor: "#f1f1f1", padding: { bottom: 10 } },
   fill: { opacity: 1 },
   tooltip: {
@@ -173,11 +170,19 @@ options = {
     },
   },
   legend: { offsetY: 7 },
+  series: jsonData.map(function(item) {
+    return {
+      name: item.categoria,
+      data: [item.conteo] // Utiliza el conteo correspondiente a cada categoría
+    };
+  }),
 };
+
 (chart = new ApexCharts(
   document.querySelector("#column_chart"),
   options
 )).render();
+
 /* fin de datos de grafica */
 options = {
   chart: { height: 350, type: "bar", toolbar: { show: !1 } },
