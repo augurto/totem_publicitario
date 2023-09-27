fcdc7e96bd741cd64e94e768f7114758e4efcab1
Ego <augurto.17@gmail.com>
2023-09-27 03:26:09 -0500

    erw

--- inicioGraficas.php
+++ inicioGraficas.php
@@ -189,7 +189,48 @@ $empresaUser =$_SESSION['empresaUser'] ;
                             </div>
                         </div>  
                         <!-- end row -->
-                
+                        <script type="text/javascript">
+                            document.addEventListener("DOMContentLoaded", function () {
+                                var jsonData = <?php echo json_encode($data); ?>;
+                                var categorias = <?php echo json_encode($categorias); ?>;
+
+                                var options = {
+                                    chart: { height: 350, type: "bar", toolbar: { show: !1 } },
+                                    plotOptions: {
+                                        bar: { horizontal: !1, columnWidth: "45%", endingShape: "rounded" },
+                                    },
+                                    dataLabels: { enabled: !1 },
+                                    stroke: { show: !0, width: 2, colors: ["transparent"] },
+                                    colors: ["#5867c3", "#34c38f", "#f9c341"],
+                                    xaxis: {
+                                        categories: categorias, // Utiliza los meses y años como etiquetas
+                                    },
+                                    yaxis: { title: { text: "$ (thousands)" } },
+                                    grid: { borderColor: "#f1f1f1", padding: { bottom: 10 } },
+                                    fill: { opacity: 1 },
+                                    tooltip: {
+                                        y: {
+                                            formatter: function (e) {
+                                                return "$ " + e + " thousands";
+                                            },
+                                        },
+                                    },
+                                    legend: { offsetY: 7 },
+                                    series: jsonData.map(function (item) {
+                                        return {
+                                            name: item.categoria,
+                                            data: [item.conteo], // Utiliza el conteo correspondiente a cada categoría
+                                        };
+                                    }),
+                                };
+
+                                var chart = new ApexCharts(
+                                    document.querySelector("#column_chart"),
+                                    options
+                                );
+                                chart.render();
+                            });
+                        </script>
 
                         <!-- FIN DATOS -->
                     </div> <!-- container-fluid -->
