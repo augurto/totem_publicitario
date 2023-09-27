3d08a195e73633d44944e401192802cccd47b320
Ego <augurto.17@gmail.com>
2023-09-27 01:33:26 -0500

    ver un div

--- inicioGraficas.php
+++ inicioGraficas.php
@@ -75,7 +75,28 @@ $empresaUser =$_SESSION['empresaUser'] ;
                     <div class="container-fluid">
                         <!-- INICIO DATOS -->
 
-                    
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4 class="card-title mb-4">Spline Area</h4>
+
+                                        <div id="spline_area" class="apex-charts" dir="ltr"></div>                      
+                                    </div>
+                                </div><!--end card-->
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4 class="card-title mb-4">Column Charts</h4>
+                                        
+                                        <div id="column_chart" class="apex-charts" dir="ltr"></div>                                      
+                                    </div>
+                                </div><!--end card-->
+                            </div>
+                        </div>
+                        <!-- end row -->
                 
 
                         <!-- FIN DATOS -->
@@ -92,6 +113,40 @@ $empresaUser =$_SESSION['empresaUser'] ;
         <!-- END layout-wrapper -->
 
         <?php include './parts/sidebar.php';?>
+        <script>
+            options = {
+            chart: { height: 350, type: "bar", toolbar: { show: !1 } },
+            plotOptions: {
+                bar: { horizontal: !1, columnWidth: "45%", endingShape: "rounded" },
+            },
+            dataLabels: { enabled: !1 },
+            stroke: { show: !0, width: 2, colors: ["transparent"] },
+            series: [
+                { name: "Net Profit", data: [46, 57, 59, 54, 62, 58, 64, 60, 66] },
+                { name: "Revenue", data: [74, 83, 102, 97, 86, 106, 93, 114, 94] },
+                { name: "Free Cash Flow", data: [37, 42, 38, 26, 47, 50, 54, 55, 43] },
+            ],
+            colors: ["#5867c3", "#34c38f", "#f9c341"],
+            xaxis: {
+                categories: ["Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+            },
+            yaxis: { title: { text: "$ (thousands)" } },
+            grid: { borderColor: "#f1f1f1", padding: { bottom: 10 } },
+            fill: { opacity: 1 },
+            tooltip: {
+                y: {
+                formatter: function (e) {
+                    return "$ " + e + " thousands";
+                },
+                },
+            },
+            legend: { offsetY: 7 },
+            };
+            (chart = new ApexCharts(
+            document.querySelector("#column_chart"),
+            options
+            )).render();
+        </script>
 
         <!-- JAVASCRIPT -->
         <script src="assets/libs/jquery/jquery.min.js"></script>
