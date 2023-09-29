3af2877b6ff4bb5701203bf72c16ac0c58220021
Ego <augurto.17@gmail.com>
2023-09-29 07:07:43 -0500

    arreglado

--- test.php
+++ test.php
@@ -68,24 +68,13 @@ include 'includes/conexion.php'; // Incluir el archivo de conexión
                         <div class="card">
                             <div class="card-body">
 
-                                <h4 class="card-title">Textual inputs</h4>
-                                <p class="card-title-desc">Here are examples of <code
-                                        class="highlighter-rouge">.form-control</code> applied to each
-                                    textual HTML5 <code class="highlighter-rouge">&lt;input&gt;</code> <code
-                                        class="highlighter-rouge">type</code>.</p>
+                                <h4 class="card-title">Prueba de pdf</h4>
+                                
                                 <div class="row mb-3">
                                     <label for="example-text-input" class="col-sm-2 col-form-label">Text</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" type="text" placeholder="Artisanal kale"
                                             id="example-text-input">
-                                    </div>
-                                </div>
-                                <!-- end row -->
-                                <div class="row mb-3">
-                                    <label for="example-search-input" class="col-sm-2 col-form-label">Search</label>
-                                    <div class="col-sm-10">
-                                        <input class="form-control" type="search" placeholder="How do I shoot web"
-                                            id="example-search-input">
                                     </div>
                                 </div>
                                 <!-- end row -->
@@ -98,26 +87,10 @@ include 'includes/conexion.php'; // Incluir el archivo de conexión
                                 </div>
                                 <!-- end row -->
                                 <div class="row mb-3">
-                                    <label for="example-url-input" class="col-sm-2 col-form-label">URL</label>
-                                    <div class="col-sm-10">
-                                        <input class="form-control" type="url" placeholder="https://getbootstrap.com"
-                                            id="example-url-input">
-                                    </div>
-                                </div>
-                                <!-- end row -->
-                                <div class="row mb-3">
                                     <label for="example-tel-input" class="col-sm-2 col-form-label">Telephone</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" type="tel" placeholder="1-(555)-555-5555"
                                             id="example-tel-input">
-                                    </div>
-                                </div>
-                                <!-- end row -->
-                                <div class="row mb-3">
-                                    <label for="example-password-input" class="col-sm-2 col-form-label">Password</label>
-                                    <div class="col-sm-10">
-                                        <input class="form-control" type="password" value="hunter2"
-                                            id="example-password-input">
                                     </div>
                                 </div>
                                 <!-- end row -->
@@ -127,47 +100,7 @@ include 'includes/conexion.php'; // Incluir el archivo de conexión
                                         <input class="form-control" type="number" value="42" id="example-number-input">
                                     </div>
                                 </div>
-                                <!-- end row -->
-                                <div class="row mb-3">
-                                    <label for="example-datetime-local-input" class="col-sm-2 col-form-label">Date and
-                                        time</label>
-                                    <div class="col-sm-10">
-                                        <input class="form-control" type="datetime-local" value="2011-08-19T13:45:00"
-                                            id="example-datetime-local-input">
-                                    </div>
-                                </div>
-                                <!-- end row -->
-                                <div class="row mb-3">
-                                    <label for="example-date-input" class="col-sm-2 col-form-label">Date</label>
-                                    <div class="col-sm-10">
-                                        <input class="form-control" type="date" value="2011-08-19"
-                                            id="example-date-input">
-                                    </div>
-                                </div>
-                                <!-- end row -->
-                                <div class="row mb-3">
-                                    <label for="example-month-input" class="col-sm-2 col-form-label">Month</label>
-                                    <div class="col-sm-10">
-                                        <input class="form-control" type="month" value="2020-03"
-                                            id="example-month-input">
-                                    </div>
-                                </div>
-                                <!-- end row -->
-                                <div class="row mb-3">
-                                    <label for="example-week-input" class="col-sm-2 col-form-label">Week</label>
-                                    <div class="col-sm-10">
-                                        <input class="form-control" type="week" value="2020-W14"
-                                            id="example-week-input">
-                                    </div>
-                                </div>
-                                <!-- end row -->
-                                <div class="row mb-3">
-                                    <label for="example-time-input" class="col-sm-2 col-form-label">Time</label>
-                                    <div class="col-sm-10">
-                                        <input class="form-control" type="time" value="13:45:00"
-                                            id="example-time-input">
-                                    </div>
-                                </div>
+                               
                                 <!-- end row -->
                                 <div class="row mb-3">
                                     <label for="example-color-input" class="col-sm-2 col-form-label">Color</label>
@@ -201,29 +134,7 @@ include 'includes/conexion.php'; // Incluir el archivo de conexión
 
                         <!-- FIN DATOS -->
 
-                        <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                                    <h4 class="mb-sm-0">Data Tables</h4>
-                                    <div class="page-title-center">
-                                    <button type="button" class="btn btn-primary waves-effect waves-light" style="height: 35px !important;" onclick="window.location.href = 'nuevoCliente.php';">
-                                        Nuevo Cliente <i class="mdi mdi-emoticon-excited-outline font-size-16 align-middle ms-2"></i>
-                                    </button>
-
-                                    </div>
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
-                                            <li class="breadcrumb-item active">Data Tables</li>
-                                        </ol>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                        <!-- end page title -->
-
+                       
                                
                         
                         
