31785f133b2f89dfb172547a1e068b52cbbb680d
Ego <augurto.17@gmail.com>
2023-10-06 19:33:16 -0500

    cambio

--- includes/buscarProducto.php
+++ includes/buscarProducto.php
@@ -5,8 +5,16 @@ if (isset($_POST['atributos'])) {
     $selectedAtributos = $_POST['atributos'];
     $atributosCondition = implode(',', $selectedAtributos);
 
+    // Moneda seleccionada (0 para Soles y 1 para Dólares)
+    $tipoMonedaSeleccionada = isset($_POST['tipoMoneda']) ? intval($_POST['tipoMoneda']) : 0;
+
+    // Definir las variables de precio y descuento según la moneda seleccionada
+    $precioColumn = ($tipoMonedaSeleccionada === 1) ? 'precioDolar' : 'Precio';
+    $precioSecundarioColumn = ($tipoMonedaSeleccionada === 1) ? 'precioDolarMin' : 'precioMin';
+    $descuentoGeneralColumn = ($tipoMonedaSeleccionada === 1) ? 'descuentoMaxDolar' : 'descuentoMax';
+
     // Consulta para obtener el último producto que cumple con la mayoría de los requisitos
-    $query = "SELECT p.ID, p.Nombre, p.Descripcion, p.Precio, p.precioDolar, p.descuentoMax, p.precioMin, p.precioDolarMin, p.descuentoMaxDolar, COUNT(pa.ID) AS contador
+    $query = "SELECT p.ID, p.Nombre, p.Descripcion, p.$precioColumn, p.$precioSecundarioColumn, p.$descuentoGeneralColumn, COUNT(pa.ID) AS contador
               FROM productos p
               INNER JOIN producto_atributos pa ON p.ID = pa.Producto_ID
               WHERE pa.Atributo_ID IN ($atributosCondition)
@@ -21,12 +29,9 @@ if (isset($_POST['atributos'])) {
             'ID' => $row['ID'],
             'Nombre' => $row['Nombre'],
             'Descripcion' => $row['Descripcion'],
-            'Precio' => $row['Precio'],
-            'precioDolar' => $row['precioDolar'],
-            'descuentoMax' => $row['descuentoMax'],
-            'precioMin' => $row['precioMin'],
-            'precioDolarMin' => $row['precioDolarMin'],
-            'descuentoMaxDolar' => $row['descuentoMaxDolar']
+            'PrecioPrincipal' => $row[$precioColumn],
+            'PrecioSecundario' => $row[$precioSecundarioColumn],
+            'DescuentoGeneral' => $row[$descuentoGeneralColumn]
         );
         echo json_encode($productoInfo); // Devuelve los datos como JSON
     } else {
@@ -35,18 +40,16 @@ if (isset($_POST['atributos'])) {
             'ID' => '',
             'Nombre' => 'Ningún producto coincide',
             'Descripcion' => '',
-            'Precio' => '',
-            'precioDolar' => '',
-            'descuentoMax' => '',
-            'precioMin' => '',
-            'precioDolarMin' => '',
-            'descuentoMaxDolar' => ''
+            'PrecioPrincipal' => '',
+            'PrecioSecundario' => '',
+            'DescuentoGeneral' => ''
         ));
     }
 
     mysqli_free_result($result);
     mysqli_close($con);
 }
+
 ?>
 
 
