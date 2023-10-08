5038216123debe45a2f07409f40ddba8ebf99e43
Ego <augurto.17@gmail.com>
2023-10-07 20:11:35 -0500

    XD

--- includes/buscarProducto.php
+++ includes/buscarProducto.php
@@ -8,13 +8,8 @@ if (isset($_POST['atributos'])) {
     // Moneda seleccionada (0 para Soles y 1 para Dólares)
     $tipoMonedaSeleccionada = isset($_POST['tipoMoneda']) ? intval($_POST['tipoMoneda']) : 0;
 
-    // Definir las variables de precio y descuento según la moneda seleccionada
-    $precioColumn = ($tipoMonedaSeleccionada === 1) ? 'precioDolar' : 'Precio';
-    $precioSecundarioColumn = ($tipoMonedaSeleccionada === 1) ? 'precioDolarMin' : 'precioMin';
-    $descuentoGeneralColumn = ($tipoMonedaSeleccionada === 1) ? 'descuentoMaxDolar' : 'descuentoMax';
-
     // Consulta para obtener el último producto que cumple con la mayoría de los requisitos
-    $query = "SELECT p.ID, p.Nombre, p.Descripcion, p.$precioColumn, p.$precioSecundarioColumn, p.$descuentoGeneralColumn, COUNT(pa.ID) AS contador
+    $query = "SELECT p.ID, p.Nombre, p.Descripcion, p.Precio, p.precioDolar, p.descuentoMax, p.precioMin, p.precioDolarMin, p.descuentoMaxDolar, COUNT(pa.ID) AS contador
               FROM productos p
               INNER JOIN producto_atributos pa ON p.ID = pa.Producto_ID
               WHERE pa.Atributo_ID IN ($atributosCondition)
@@ -25,13 +20,18 @@ if (isset($_POST['atributos'])) {
     $result = mysqli_query($con, $query);
 
     if ($row = mysqli_fetch_assoc($result)) {
+        // Definir las variables de precio y descuento según la moneda seleccionada
+        $precioPrincipal = ($tipoMonedaSeleccionada === 1) ? $row['precioDolar'] : $row['Precio'];
+        $precioSecundario = ($tipoMonedaSeleccionada === 1) ? $row['precioDolarMin'] : $row['precioMin'];
+        $descuentoGeneral = ($tipoMonedaSeleccionada === 1) ? $row['descuentoMaxDolar'] : $row['descuentoMax'];
+
         $productoInfo = array(
             'ID' => $row['ID'],
             'Nombre' => $row['Nombre'],
             'Descripcion' => $row['Descripcion'],
-            'PrecioPrincipal' => $row[$precioColumn],
-            'PrecioSecundario' => $row[$precioSecundarioColumn],
-            'DescuentoGeneral' => $row[$descuentoGeneralColumn]
+            'PrecioPrincipal' => $precioPrincipal,
+            'PrecioSecundario' => $precioSecundario,
+            'DescuentoGeneral' => $descuentoGeneral
         );
         echo json_encode($productoInfo); // Devuelve los datos como JSON
     } else {
@@ -49,6 +49,7 @@ if (isset($_POST['atributos'])) {
     mysqli_free_result($result);
     mysqli_close($con);
 }
+
 
 ?>
 
