2dd73f7c1b7aeb3921cee0087cc80b5dcc982a24
Ego <augurto.17@gmail.com>
2023-11-14 11:58:19 -0500

    aer pdf

--- seguimientoClienteCotizadoPDF.php
+++ seguimientoClienteCotizadoPDF.php
@@ -1,6 +1,9 @@
 <?php
-require('tcpdf/tcpdf.php');
-require('includes/conexion.php');
+require './dompdf/src/Dompdf.php';
+require 'includes/conexion.php';
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 // Obtener el ID de la URL
 if (isset($_GET['id'])) {
@@ -9,100 +12,50 @@ if (isset($_GET['id'])) {
     die("ID no proporcionado en la URL");
 }
 
-// Crear una instancia de la clase PDF
-$pdf = new TCPDF();
-$pdf->AddPage();
+// Crear una instancia de la clase Dompdf
+$options = new Options();
+$options->set('isHtml5ParserEnabled', true);
+$options->set('isPhpEnabled', true);
 
-// Iniciar la tabla con bordes de color #009ecd
-$html = '<table style="border: 1px solid #009ecd;"><tr>';
+$dompdf = new Dompdf($options);
 
-// Primera columna con una imagen centrada horizontal y verticalmente y padding superior de 20px
-$html .= '<td style="border: 1px solid #009ecd; text-align: center; vertical-align: middle; padding-top: 20px;">';
+// ...
 
-// Añadir la imagen
-$imagePath = 'assets/images/logogeosatelital.jpg';
-$html .= '<img src="' . $imagePath . '" width="90" height="26" />';
-
-$html .= '</td>';
-
-// Segunda columna con tres filas, cada una con borde de color #009ecd y color de texto #009ecd
-$html .= '<td style="border: 1px solid #009ecd; text-align: center; padding-top: 20px;">';
-$html .= '<div style="border-bottom: 1px solid #009ecd; color: #009ecd;">20600137094</div>';
-$html .= '<div style="border-bottom: 1px solid #009ecd; color: #009ecd;">T.(01)739-0556</div>';
-$html .= '<div style="color: #009ecd;">Av. Industrial 3220 - Independencia</div>';
-$html .= '</td>';
-
-// Tercera columna con dos filas, cada una con borde de color #009ecd y color de texto #009ecd
-$html .= '<td style="border: 1px solid #009ecd; text-align: center; padding-top: 20px;">';
-$html .= '<div style="border-bottom: 1px solid #009ecd; color: #009ecd;">Nro Cotizacion 2023 - ' . $id . '</div>';
-$html .= '<div style="color: #009ecd;">www.geosatelital.com</div>';
-$html .= '</td>';
-
-// Cerrar la tabla
-$html .= '</tr></table>';
-
-// Insertar la tabla en el PDF
-$pdf->writeHTML($html, true, false, true, false, '');
-
-// Nombre del archivo PDF de salida
-
-// Mostrar el ID
-$pdf->Cell(0, 10, "ID: $id", 0, 1);
-
-// Consultar los datos de los productos
-$query = "SELECT nombreProducto, moneda, precioPrincipal, precioSecundario, cantidad, descuentoMonto, descuentoMaximo, subtotal
-          FROM tabla_productos
-          WHERE id_form_web = $id";
-$result = mysqli_query($con, $query);
+// Consultar los datos del cliente
+$queryCliente = "SELECT nombres, apellidoPaterno, apellidoMaterno FROM tabla_clientes WHERE id_cliente = $id";
+$resultCliente = mysqli_query($con, $queryCliente);
 
 // Verificar si se obtuvieron resultados
-if ($result && mysqli_num_rows($result) > 0) {
-    $pdf->Ln(10);
+if ($resultCliente && mysqli_num_rows($resultCliente) > 0) {
+    $clienteData = mysqli_fetch_assoc($resultCliente);
 
-    // Abreviaciones en la cabecera de la tabla
-    $pdf->Cell(35, 10, 'Nombre Prod.', 1);
-    $pdf->Cell(25, 10, 'Moneda', 1);
-    $pdf->Cell(35, 10, 'Precio Principal', 1);
-    $pdf->Cell(35, 10, 'Precio Sec.', 1);
-    $pdf->Cell(25, 10, 'Cantidad', 1);
-    $pdf->Cell(35, 10, 'Desc. Monto', 1);
-    $pdf->Cell(35, 10, 'Desc. Máx.', 1);
-    $pdf->Cell(35, 10, 'Subtotal', 1);
-    $pdf->Ln();
+    // Obtener nombre y apellidos del cliente
+    $nombreCliente = $clienteData['nombres'];
+    $apellidoPaternoCliente = $clienteData['apellidoPaterno'];
+    $apellidoMaternoCliente = $clienteData['apellidoMaterno'];
 
-    while ($row = mysqli_fetch_assoc($result)) {
-        // Datos de productos
-        $pdf->Cell(35, 10, $row['nombreProducto'], 1);
-        $pdf->Cell(25, 10, $row['moneda'], 1);
-        $pdf->Cell(35, 10, $row['precioPrincipal'], 1);
-        $pdf->Cell(35, 10, $row['precioSecundario'], 1);
-        $pdf->Cell(25, 10, $row['cantidad'], 1);
-        $pdf->Cell(35, 10, $row['descuentoMonto'], 1);
-        $pdf->Cell(35, 10, $row['descuentoMaximo'], 1);
-        $pdf->Cell(35, 10, $row['subtotal'], 1);
-        $pdf->Ln();
-    }
+    // Concatenar nombre y apellidos
+    $nombresApellidosCliente = "$nombreCliente $apellidoPaternoCliente $apellidoMaternoCliente";
+
+    // Generar el contenido HTML con la información del cliente
+    $htmlCliente = '<h1>Información del Cliente</h1>';
+    $htmlCliente .= '<p>' . $nombresApellidosCliente . '</p>';
+
+    // Cargar el contenido HTML en dompdf
+    $dompdf->loadHtml($htmlCliente);
+
+    // Establecer el tamaño del papel y la orientación
+    $dompdf->setPaper('A4', 'portrait');
+
+    // Renderizar el contenido HTML en PDF
+    $dompdf->render();
+
+    // Nombre del archivo PDF de salida
+    $pdfFileName = "cliente_informacion.pdf";
+
+    // Descargar el archivo PDF
+    $dompdf->stream($pdfFileName, array('Attachment' => 0));
 } else {
-    $pdf->Ln(10);
-    $pdf->Cell(0, 10, 'No se encontraron productos para este ID.', 0, 1);
+    die("No se encontró información del cliente para este ID.");
 }
-
-// Agregar una nueva página
-$pdf->AddPage();
-
-// Insertar la imagen en la nueva página (segunda página) y ajustar el tamaño para que ocupe toda la página
-$pdf->Image('assets/images/geoprime1.jpg', 10, 10, 190);
-
-// Agregar una nueva página
-$pdf->AddPage();
-
-// Insertar la imagen en la nueva página (tercera página) y ajustar el tamaño para que ocupe toda la página
-$pdf->Image('assets/images/geoprime2.jpg', 10, 10, 190);
-
-// Nombre del archivo PDF de salida
-$pdfFileName = "productos.pdf";
-
-// Generar el PDF y mostrarlo en el navegador
-$pdf->Output($pdfFileName, 'I');
 ?>
-
