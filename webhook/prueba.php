02f87ff94524f8cc6c17fc9b53f4a5909b9c650c
Ego <augurto.17@gmail.com>
2023-11-28 18:28:31 -0500

    asdasd

--- webhook/prueba.php
+++ webhook/prueba.php
@@ -10,8 +10,9 @@ $data = file_get_contents("php://input");
 parse_str($data, $formData);
 
 // Preparar la consulta SQL para insertar datos en la tabla
-$query = "INSERT INTO formulario_prueba ( nombres, telefono) 
-          VALUES ('{$formData['Nombres']}', '{$formData['Telefono']}')";
+$query = "INSERT INTO formulario_prueba (id_formulario, nombres, email, telefono, mensaje, documento, id_form, nombre_form) 
+          VALUES (NULL, 'Juan Pérez', 'juan@example.com', '987654321', 'Este es un mensaje de prueba', '123456789', '6cf42fa', 'Prueba')";
+
 
 // Ejecutar la consulta
 $result = mysqli_query($con, $query);
