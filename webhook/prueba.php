dcbeda67299ebf46b3f6024d24b7d2c90462bf7a
Ego <augurto.17@gmail.com>
2023-11-28 18:30:57 -0500

    asd

--- webhook/prueba.php
+++ webhook/prueba.php
@@ -6,8 +6,7 @@ include './includes/conexion.php';
 // Recibir datos del webhook
 $data = file_get_contents("php://input");
 
-// Parsear la cadena de consulta
-parse_str($data, $formData);
+
 
 // Preparar la consulta SQL para insertar datos en la tabla
 $query = "INSERT INTO formulario_prueba (id_formulario, nombres, email, telefono, mensaje, documento, id_form, nombre_form) 
@@ -17,12 +16,6 @@ $query = "INSERT INTO formulario_prueba (id_formulario, nombres, email, telefono
 // Ejecutar la consulta
 $result = mysqli_query($con, $query);
 
-// Verificar si la consulta fue exitosa
-if ($result) {
-    echo "Datos insertados correctamente en la base de datos.";
-} else {
-    echo "Error al insertar datos: " . mysqli_error($con);
-}
 
 // Cerrar la conexión
 mysqli_close($con);
