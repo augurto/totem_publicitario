a3392e6adb7d2ee13d2a8498bea879ccf91d695a
Ego <augurto.17@gmail.com>
2023-11-28 17:41:17 -0500

    isnertar

--- webhook/prueba.php
+++ webhook/prueba.php
@@ -1,6 +1,28 @@
+
 <?php
+// Incluir tu archivo de conexión
+include './includes/conexion.php';
+
 // Recibir datos del webhook
 $data = file_get_contents("php://input");
-echo $data;
 
+// Parsear la cadena de consulta
+parse_str($data, $formData);
+
+// Preparar la consulta SQL para insertar datos en la tabla
+$query = "INSERT INTO formulario_prueba (id_formulario, nombres, email, telefono, mensaje, documento, id_form, nombre_form) 
+          VALUES (NULL, '{$formData['Nombres']}', '{$formData['Email']}', '{$formData['Telefono']}', '{$formData['Mensaje']}', '{$formData['Documento']}', '{$formData['form_id']}', '{$formData['forx|rueba']}')";
+
+// Ejecutar la consulta
+$result = mysqli_query($con, $query);
+
+// Verificar si la consulta fue exitosa
+if ($result) {
+    echo "Datos insertados correctamente en la base de datos.";
+} else {
+    echo "Error al insertar datos: " . mysqli_error($con);
+}
+
+// Cerrar la conexión
+mysqli_close($con);
 ?>
