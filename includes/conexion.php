15b70e7b5828fce84b0a16d39ce3a189fd7c1c97
Ego <augurto.17@gmail.com>
2023-11-28 17:43:06 -0500

    conexion

--- includes/conexion.php
+++ includes/conexion.php
@@ -4,6 +4,13 @@
    define('DB_PASSWORD', '21.17.Prueba');
    define('DB_DATABASE', 'u291982824_prueba');
    
-   $con=mysqli_connect('localhost','u291982824_prueba','21.17.Prueba','u291982824_prueba');
+   $con = mysqli_connect('localhost', 'u291982824_prueba', '21.17.Prueba', 'u291982824_prueba');
    mysqli_set_charset($con, "utf8");
+
+   // Probar la conexión
+   if (mysqli_connect_errno()) {
+       echo "Error al conectar a la base de datos: " . mysqli_connect_error();
+   } else {
+       echo "Conexión exitosa a la base de datos.";
+   }
 ?>
