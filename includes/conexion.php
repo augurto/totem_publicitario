9b3b18f8359eeb804ea7fc79825bb656ac573f0b
Ego <augurto.17@gmail.com>
2023-12-14 10:22:41 -0500

    historial de eventos

--- includes/conexion.php
+++ includes/conexion.php
@@ -11,6 +11,6 @@
    if (mysqli_connect_errno()) {
        echo "Error al conectar a la base de datos: " . mysqli_connect_error();
    } else {
-       echo "Conexión exitosa a la base de datos...";
+       echo "Historial de eventos.";
    }
 ?>
