845b3ebc10aa18d0873e88e2f95854afba18a40b
Ego <augurto.17@gmail.com>
2024-01-01 18:46:01 -0500

    fehca peru

--- email/enviar.php
+++ email/enviar.php
@@ -1,6 +1,9 @@
 <?php
 // Incluir el archivo de conexión
-include('../includes/conexion.php');
+include('conexion.php');
+
+// Establecer la zona horaria a "America/Lima"
+date_default_timezone_set('America/Lima');
 
 // Recibir datos del formulario
 $para = mysqli_real_escape_string($con, $_POST['para']);
@@ -9,13 +12,17 @@ $mensaje = mysqli_real_escape_string($con, $_POST['area']);
 
 // Procesar archivo adjunto
 $nombreArchivo = $_FILES['adjunto']['name'];
-$rutaArchivo = "archivos_email/" . $nombreArchivo;
+$rutaArchivo = "carpeta_destino/" . $nombreArchivo;
 
 // Mover el archivo al directorio de destino
 move_uploaded_file($_FILES['adjunto']['tmp_name'], $rutaArchivo);
 
+// Obtener la fecha y hora actual de Perú
+$fechaEnvio = date('Y-m-d H:i:s');
+
 // Insertar datos en la base de datos
-$sql = "INSERT INTO mensajes (para, asunto, mensaje, archivo_adjunto) VALUES ('$para', '$asunto', '$mensaje', '$nombreArchivo')";
+$sql = "INSERT INTO mensajes (para, asunto, mensaje, archivo_adjunto, fecha_envio) 
+        VALUES ('$para', '$asunto', '$mensaje', '$nombreArchivo', '$fechaEnvio')";
 
 if (mysqli_query($con, $sql)) {
     echo "Mensaje enviado y almacenado en la base de datos con éxito";
