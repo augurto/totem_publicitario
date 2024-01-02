ac74af5542de056c91a4bb7243182dd6250bdb4f
Ego <augurto.17@gmail.com>
2024-01-01 22:14:25 -0500

    ocultando iconos

--- email_enviar.php
+++ email_enviar.php
@@ -74,12 +74,12 @@ $empresaUser = $_SESSION['empresaUser'];
                     <div class="row">
                         <div class="col-12">
                             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                                <h4 class="mb-sm-0">Inbox</h4>
+                                <h4 class="mb-sm-0">Email</h4>
 
                                 <div class="page-title-right">
                                     <ol class="breadcrumb m-0">
                                         <li class="breadcrumb-item"><a href="javascript: void(0);">Email</a></li>
-                                        <li class="breadcrumb-item active">Inbox</li>
+                                        <li class="breadcrumb-item active">Enviados</li>
                                     </ol>
                                 </div>
 
@@ -119,7 +119,7 @@ $empresaUser = $_SESSION['empresaUser'];
                                             <button type="button" class="btn btn-primary waves-light waves-effect"><i class="fa fa-exclamation-circle"></i></button>
                                             <button type="button" class="btn btn-primary waves-light waves-effect"><i class="far fa-trash-alt"></i></button>
                                         </div>
-                                        <div class="btn-group me-2 mb-2 mb-sm-0">
+                                        <!-- <div class="btn-group me-2 mb-2 mb-sm-0">
                                             <button type="button" class="btn btn-primary waves-light waves-effect dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                                                 <i class="fa fa-folder"></i> <i class="mdi mdi-chevron-down ms-1"></i>
                                             </button>
@@ -151,7 +151,7 @@ $empresaUser = $_SESSION['empresaUser'];
                                                 <a class="dropdown-item" href="#">Add Star</a>
                                                 <a class="dropdown-item" href="#">Mute</a>
                                             </div>
-                                        </div>
+                                        </div> -->
                                     </div>
                                     <ul class="message-list">
                                         <?php
@@ -218,7 +218,7 @@ $empresaUser = $_SESSION['empresaUser'];
 
                                 <div class="row">
                                     <div class="col-7">
-                                        Showing 1 - 20 of 1,524
+                                        Mostrando 1 - 20 de 1,524
                                     </div>
                                     <div class="col-5">
                                         <div class="btn-group float-end">
