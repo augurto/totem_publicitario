82add6fd461f2d709a550e1710ab7929395bf38a
Ego <augurto.17@gmail.com>
2024-01-03 09:42:04 -0500

    cambio de titulo 2

--- vendedor.php
+++ vendedor.php
@@ -24,7 +24,7 @@ $empresaUser =$_SESSION['empresaUser'] ;
     <head>
         
         <meta charset="utf-8" />
-        <title>Ego <?php echo "<3"; ?></title>
+        <title>GEO <?php echo "<3"; ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
         <meta content="Themesdesign" name="author" />
